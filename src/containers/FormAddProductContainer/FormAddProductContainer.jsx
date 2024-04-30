a76c300be8e19e9a60224ca58f7e067b062cad50
valentinoaraya <valentinoaraya04@gmail.com>
2024-04-30 00:49:26 -0300

    Los usuarios administradores pueden subir productos a la base de datos

---
+++ src/containers/FormAddProductContainer/FormAddProductContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./FormAddProductContainer.css"
+import FormAddProduct from '../../components/FormAddProduct/FormAddProduct.jsx';
+
+const FormAddProductContainer = () => {
+    return (
+        <div className='divFormAddProductContainer'>
+            <FormAddProduct/>
+        </div>
+    );
+}
+
+export default FormAddProductContainer;
